b0f81432a2ed4c5efe7848b59959c4ca88b63a84
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-02-27 13:28:32 +0200

    Posts displayed successfully

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require "functions.php";
+
 echo "Hi <br>";
 
 $connection_string = "mysql:host=localhost;dbname=blog_IPb22;user=root;password=root;charset=utf8mb4";
@@ -7,12 +9,16 @@ $pdo = new PDO($connection_string);
 
 // 1. Sagatavot SQL izpildei
 $query = $pdo->prepare("SELECT * FROM posts");
+
 // 2. Izpilīt SQL
 $query->execute();
 // 3. Beidzot dabūt rezultātus, visus pie tam!
 $posts = $query->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<pre>";
-var_dump($posts);
-echo "</pre>";
+echo "<ul>";
+foreach($posts as $post) {
+  echo "<li>" . $post["title"] . "</li>";
+}
+echo "</ul>";
+
 
