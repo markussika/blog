7d3cd30fa99d0c12347bfe9e080e97d22458c170
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-03-04 15:18:47 +0200

    URL parameter added with form

--- index.php
+++ index.php
@@ -6,10 +6,25 @@ require "Database.php";
 
 $config = require("config.php");
 
+$query = "SELECT * FROM posts";
+if (isset($_GET["id"]) && $_GET["id"] != "") {
+  $id = $_GET["id"];
+  $query = "SELECT * FROM posts WHERE id=$id";
+}
+
+
 $db = new Database($config);
 $posts = $db
-          ->execute("SELECT * FROM posts")
+          ->execute($query)
           ->fetchAll();
+
+
+echo "<form>";
+echo "<input name='id'/>";
+echo "<button>Submit</button>";
+echo "</form>";
+
+echo "<h1>Posts</h1>";
 
 echo "<ul>";
 foreach($posts as $post) {
