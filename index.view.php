90f58b97fa443015bbc21832cb97887c13e2f2b9
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-03-12 13:31:04 +0200

    Logic seperated from view

---
+++ index.view.php
@@ -0,0 +1,28 @@
+<form>
+  <input name='id' value='<?= ($_GET["id"] ?? "") ?>'/>
+  <button>Filter by ID</button>
+</form>
+
+<form>
+  <input name='category' value='<?= ($_GET["category"] ?? "") ?>'/>
+  <button>Filter by category</button>
+</form>
+
+<form>
+  Category:
+  <select name='category'>
+    <option value='sport'>Sport</option>
+    <option value='music'>Music</option>
+    <option value='food'>Food</option>
+  </select>
+  <br>
+  <button>Submit</button>
+</form>
+
+<h1>Posts</h1>
+
+<ul>
+<?php foreach($posts as $post) { ?>
+  <li> <?= $post["title"]?> </li>
+<?php } ?>
+</ul>
