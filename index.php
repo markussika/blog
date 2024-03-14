85e5f18b42775581a5d217adaff391c9a070e6f9
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-03-14 13:10:37 +0200

    404 added with valid status code

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-
 require "functions.php";
 
 $url_array = parse_url($_SERVER["REQUEST_URI"]);
+
 $url = $url_array["path"];
 
 
@@ -10,16 +10,11 @@ $url = $url_array["path"];
 // tad gribam parādīt /controllers/index.php
 if ($url == "/") {
   require "controllers/index.php";
-}
-
-// Ja cilvēks iet uz /about,
-// tad gribam parādīt /controllers/about.php
-if ($url == "/about") {
+} else if ($url == "/about") {
   require "controllers/about.php";
-}
-
-// Ja cilvēks iet uz /story,
-// tad gribam parādīt /controllers/story.php
-if ($url == "/story") {
+} else if ($url == "/story") {
   require "controllers/story.php";
+} else {
+  http_response_code(404);
+  require "controllers/404.php";
 }
