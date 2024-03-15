7305bec488fb6a3df09c1a6dc61154380016d1cc
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-03-15 09:11:32 +0200

    Router and routes in seperate files

--- index.php
+++ index.php
@@ -1,20 +1,8 @@
 <?php
-require "functions.php";
 
-$url_array = parse_url($_SERVER["REQUEST_URI"]);
+require "router.php";
 
-$url = $url_array["path"];
-
-
-// Ja cilvēks iet uz /,
-// tad gribam parādīt /controllers/index.php
-if ($url == "/") {
-  require "controllers/index.php";
-} else if ($url == "/about") {
-  require "controllers/about.php";
-} else if ($url == "/story") {
-  require "controllers/story.php";
-} else {
-  http_response_code(404);
-  require "controllers/404.php";
-}
+// Lietotājs ir ierakstījis pārlūkprogrammā
+// localhost/about
+// Kuras datnes un kādā secībā tiek izsauktas?
+// Jātiek līdz HTML, kas tiks sūtīts atbildē
