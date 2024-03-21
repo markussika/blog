9995a89e8fb25ed1e0875e0c980504a527db174c
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-03-21 13:52:18 +0200

    Posts saved to DB

--- controllers/posts-create.php
+++ controllers/posts-create.php
@@ -1,10 +1,29 @@
 <?php
 
+require "Database.php";
+$config = require("config.php");
+$db = new Database($config);
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  
   $query = "INSERT INTO posts (title, category_id)
             VALUES (:title, :category_id);";
+  $params = [
+    ":title" => $_POST["title"],
+    ":category_id" => $_POST["category-id"]
+  ];
+  $db->execute($query, $params);
+
+  header("Location: /");
+  die();
+
+
+  // Ja viss iet,
+  // tad izveido šādu pasākumu un
+  // kolektīvu pievienošanu
+  // CKC projektā
 }
 
 $title = "Create a Post";
 require "views/posts-create.view.php";
+
